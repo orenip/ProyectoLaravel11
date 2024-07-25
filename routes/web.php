a3007a1dc4d6c9daf169a27b5c606144a365559c
orenip <josanlopi@hotmail.com>
2024-07-25 16:46:01 +0200

    commit inicial

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "pagina de inicio";
 });
+
+Route::get('/posts', function () {
+    return "aqui se mostraran todos los posts";
+});
+// EL ORDEN ES IMPORTANTE SI HAY UNA VARIABLE DEBE ESTAR DEBAJO SINO NO EJECUTA ESTA LINEA
+Route::get('/posts/create', function () {
+    return "aqui se mostrara un formulario para crear un nuevo post";
+});
+// {contenido variable} debemos indicar misma variable a la funcion
+Route::get('/posts/{post}', function ($post) {
+    return "aqui se mostrara el un nuevo post {$post}";
+});
+
+//Añadir mas contenido variable
+// Route::get('/posts/{post}/{category}', function ($post, $category) {
+//     return "aqui se mostrara el un nuevo post {$post} de la categoria {$category}";
+// });
+//Si le ponemos ? indicamos que es opcional, sustituiria a ambas de arriba
+// Route::get('/posts/{post}/{category?}', function ($post, $category=null) {
+//     if($category){
+//         return "aqui se mostrara el un nuevo post {$post} de la categoria {$category}";
+//     }   
+//     return "aqui se mostrara el un nuevo post {$post} sin categoria";
+// });
